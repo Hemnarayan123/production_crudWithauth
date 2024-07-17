6d7e9095b62dd1c31ffb575d0350f6c5a10ec757
Hemnarayan <vy2756943@gmail.in>
2024-07-17 11:32:36 +0530

    changes for production

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { router, todo } from './routers/index.js';
+import path from 'path'
 
 
 
@@ -8,6 +9,16 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
+
+//............................code for development................
+if(process.env.NODE_ENV === "production"){
+    const dirPath = path.resolve();
+    app.use(express.static('./Client/dist'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(dirPath, 'Client/dist/index.html'));
+    });
+}
+
 
 
 app.use("/api/v1", router)
